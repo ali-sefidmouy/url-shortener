9be006c37231908dc2bdcad767ec21530b203e7c
Ali Sefidmouy <sefidmou@hotmail.com>
2025-07-10 18:11:17 +0330

    feat: add grpc methods

---
+++ grpc_server.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"net"
+	"url-shortener/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type urlShortenerServer struct {
+	proto.UnimplementedURLShortenerServer
+}
+
+// gRPC Shorten method
+func (s *urlShortenerServer) Shorten(ctx context.Context, req *proto.ShortenRequest) (*proto.ShortenResponse, error) {
+	original := req.GetUrl()
+	if original == "" {
+		return nil, status.Error(codes.InvalidArgument, "URL cannot be empty")
+	}
+
+	var existingCode string
+	err := DB.QueryRow("SELECT short_code FROM urls WHERE original_url = $1", original).Scan(&existingCode)
+	if err == nil {
+		return &proto.ShortenResponse{ShortUrl: "http://localhost:8080/" + existingCode}, nil
+	} else if err != sql.ErrNoRows {
+		return nil, status.Error(codes.Internal, "database error")
+	}
+
+	var short string
+	for {
+		short = generateShortURL()
+		err = DB.QueryRow("SELECT short_code FROM urls WHERE short_code = $1", short).Scan(&existingCode)
+		if err == sql.ErrNoRows {
+			break // is unique, so break
+		}
+	}
+
+	_, err = DB.Exec("INSERT INTO urls (original_url, short_code) VALUES ($1, $2)", original, short)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "could not insert URL")
+	}
+
+	return &proto.ShortenResponse{ShortUrl: "http://localhost:8080/" + short}, nil
+}
+
+// gRPC Resolve method
+func (s *urlShortenerServer) Resolve(ctx context.Context, req *proto.ResolveRequest) (*proto.ResolveResponse, error) {
+	code := req.GetShortCode()
+	if code == "" {
+		return nil, status.Error(codes.InvalidArgument, "short code cannot be empty")
+	}
+
+	var original string
+	err := DB.QueryRow("SELECT original_url FROM urls WHERE short_code = $1", code).Scan(&original)
+	if err == sql.ErrNoRows {
+		return nil, status.Error(codes.NotFound, "short code not found")
+	} else if err != nil {
+		return nil, status.Error(codes.Internal, "database error")
+	}
+
+	return &proto.ResolveResponse{OriginalUrl: original}, nil
+}
+
+func startGRPCServer() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	proto.RegisterURLShortenerServer(grpcServer, &urlShortenerServer{})
+
+	log.Println("gRPC server listening on :50051")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve gRPC: %v", err)
+	}
+}
