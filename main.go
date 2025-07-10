c0efa09ead3d18467bed8612b0439c6845ddd37f
Ali Sefidmouy <sefidmou@hotmail.com>
2025-07-10 16:15:56 +0330

    feat: add functionality to save and restore urls to DB

--- main.go
+++ main.go
@@ -1,11 +1,44 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"net/http"
+	"database/sql"
+	"fmt"
+	"log"
 	"math/rand"
+	"net/http"
+	"os"
 	"time"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+var db *sql.DB
+
+func initDB() {
+	envErr := godotenv.Load()
+	if envErr != nil {
+		log.Fatal("Error while loading .env file")
+	}
+
+	var err error
+	connStr := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Failed to connect to DB:", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatal("DB not reachable:", err)
+	}
+}
 
 var urlStore = make(map[string]string)
 
@@ -28,6 +61,9 @@ func generateShortURL() string {
 
 
 func main() {
+	initDB()
+	defer db.Close()
+
 	r := gin.Default()
 
 	r.POST("/shorten", func(c *gin.Context) {
@@ -35,13 +71,26 @@ func main() {
 			URL string `json:"url"`
 		}
 
-		if err := c.BindJSON(&req); err != nil || req.URL == "" {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid URL"})
+		if err := c.ShouldBindJSON(&req); err != nil || req.URL == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
+			return
+		}
+
+		var existingCode string
+		err := db.QueryRow("SELECT short_code FROM urls WHERE original_url = $1", req.URL).Scan(&existingCode)
+
+		if err == nil {
+			c.JSON(http.StatusOK, gin.H{"short_url": "http://localhost:8080/" + existingCode})
 			return
 		}
 
 		short := generateShortURL()
-		urlStore[short] = req.URL
+		_, err = db.Exec("INSERT INTO urls (original_url, short_code) VALUES ($1, $2)", req.URL, short)
+		
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "could not save URL"})
+			return
+		}
 
 		c.JSON(http.StatusOK, gin.H{
 			"short_url": "http://localhost:8080/" + short,
@@ -50,11 +99,15 @@ func main() {
 
 	r.GET("/:short", func(c *gin.Context) {
 		short := c.Param("short")
-		original, ok := urlStore[short]
-		if !ok {
+		var original string
+		
+		err := db.QueryRow("SELECT original_url FROM urls WHERE short_code = $1", short).Scan(&original)
+
+		if err != nil {
 			c.JSON(http.StatusNotFound, gin.H{"error": "URL not found!"})
 			return
 		}
+
 		c.Redirect(http.StatusFound, original)
 	})
 
